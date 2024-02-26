4b6452f37d32d5f5c82cca3e9f592249882dda8b
canoe726 <canoe918@gmail.com>
2024-02-26 23:44:54 +0900

    feat: multi thread project

---
+++ multi_threads/app/src/main/java/multi_threads/race_condition/RaceCondition.java
@@ -0,0 +1,32 @@
+package main.java.multi_threads.race_condition;
+
+public class RaceCondition {
+
+  int sharedRecource = 0;
+
+  public int getShardRecource() {
+    return sharedRecource;
+  }
+
+  public void startTwoThreads() {
+    Thread t1 = new Thread(
+      new Runnable() {
+        @Override
+        public void run() {
+          sharedRecource += 1;
+        }
+      }
+    );
+    t1.start();
+
+    Thread t2 = new Thread(
+      new Runnable() {
+        @Override
+        public void run() {
+          sharedRecource -= 1;
+        }
+      }
+    );
+    t2.start();
+  }
+}
