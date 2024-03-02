720abd3a2089503406514669891e3ca1e0b7fc86
YoungBaeKim <canoe918@gmail.com>
2024-03-02 21:08:01 +0900

    feat: new pages

--- ThreadApplication/app/src/main/java/com/example/threadapplication/MainActivity.java
+++ ThreadApplication/app/src/main/java/com/example/threadapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.threadapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Process;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int count = 0;
+                for (long i = 0; i < 400000000L; i++) {
+                    count += 1;
+                }
+                System.out.println("count : " + count + "\n");
+            }
+        }, "Thread1");
+        thread1.start();
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SubActivity.class));
+            }
+        });
     }
 }
